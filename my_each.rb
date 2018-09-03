4246a60479bc67dab91fb7ac8380c4ab737a313a
Rob Parker <into.the.parker@gmail.com>
2018-09-03 09:57:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
-end
+  def my_each(array)
+    i=0 
+    while i < array.length
+      yield array[i]
+      i += 1  
+    end 
+  end 
+
+
+  collection = [1,2,3,4]
+
+  my_each(collection) { |i| puts i}
